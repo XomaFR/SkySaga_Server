fd94c38f7290ae3a4693f76b43b33e38e3d198c4
XomaFR <56118950+XomaFR@users.noreply.github.com>
2025-03-07 19:42:57 +0100

    Create autoexec.cmd

---
+++ autoexec.cmd
@@ -0,0 +1,16 @@
+auth="Test123"
+
+ws_host="localhost"
+ws_port=5164
+
+allowim=1
+devimip="localhost"
+manport=5164
+
+multiApp=1
+
+useAnalytics=0
+
+windowtitle="SkySaga: Infinite Isles | EDITz"
+
+ivar="timedelta=10"
